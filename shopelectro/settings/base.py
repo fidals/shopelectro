db1f80d7ee16a5d58205bc6fcd7940074fb9f7bf
wrtds2 <wertins71@gmail.com>
2016-06-17 14:18:01 +0300

    Moved logo from png to svg

--- shopelectro/settings/base.py
+++ shopelectro/settings/base.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'shopelectro.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'refarm.sqlite3'
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': 'refarm.sqlite3'
+   },
 }
 
 # Password validation
