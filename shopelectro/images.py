3b537aabdf1ff16698a9c0dd380569edc83addaa
Biriukov Egor <birykovegor@gmail.com>
2016-07-11 16:21:22 +0300

    Upload files with thumbnails.

--- shopelectro/images.py
+++ shopelectro/images.py
@@ -4,7 +4,49 @@ from media root.
 """
 
 import os
+from PIL import Image
+
 from django.conf import settings
+
+DEFAULT_SIZE = 800, 800
+PREVIEW_SIZE = 300, 300
+
+def upload(type_, id_, files):
+    """Upload image for an entity of a given type_ with given id_."""
+    def create_file(file, dir, name=None, size=DEFAULT_SIZE):
+        """
+        Create file (physically) in a given directory.
+
+        Accepts:
+                file - InMemoryFileObject
+                dir - path to directory where the file will be saved
+                name - non-default name for a file.
+                       If not specified, file will be saved with uploaded name.
+                size - tuple of sizes. File will be resized if its sizes > given size.
+        """
+        uploaded_name, ext = str(file).split('.')
+        name = name or uploaded_name
+
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+        image = Image.open(file)
+
+        if image.size > size:
+            image.resize(size)
+
+        return image.save(os.path.join(dir, '{}.{}'.format(name, ext)))
+
+    def is_main_image(file):
+        """Main image is an image which name contains 'main' substring."""
+        return 'main' in str(file)
+
+    upload_dir = os.path.join(settings.MEDIA_ROOT, '{}/{}'.format(type_, id_))
+
+    for file in files:
+        create_file(file, upload_dir)
+        if is_main_image(file):
+            create_file(file, upload_dir, name='small', size=PREVIEW_SIZE)
 
 
 def get_all_images(model, url='products'):
