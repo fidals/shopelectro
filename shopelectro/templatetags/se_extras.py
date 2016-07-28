2b2a3baf83cb58530bd16ddfabf7085c36d89da1
kyrsant34 <kyrsant34@gmail.com>
2016-07-28 14:29:28 +0300

    Corrected comments

--- shopelectro/templatetags/se_extras.py
+++ shopelectro/templatetags/se_extras.py
@@ -19,14 +19,17 @@ register = template.Library()
 def roots():
     return Category.objects.root_nodes().order_by('position')
 
+
+# Return a random product category and all its subcategories.
 @register.simple_tag
 def random_product(category):
     products, count = category.get_recursive_products_with_count(size=None)
     if not products:
-        return ''
+        return
     product = products[random.randint(0, count - 1)]
     product.image = images.get_image(product, settings.IMAGES['small'])
     return product
+
 
 @register.filter
 def class_name(model):
