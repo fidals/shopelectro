c274ab122798708114aa95797472560063602d55
duker33 <duker33@gmail.com>
2017-03-21 19:38:28 +0300

    Refactor update_meta_tags command

--- shopelectro/management/commands/update_meta_tags.py
+++ shopelectro/management/commands/update_meta_tags.py
@@ -5,9 +5,10 @@ from django.core.management.base import BaseCommand
 
 from shopelectro.models import ProductPage, CategoryPage, Product
 
-CATEGORY_TITLE = '{0.name} - купить в интернет-магазине - ShopElectro'
+
+CATEGORY_TITLE = '{name} - купить в интернет-магазине - ShopElectro'
 CATEGORY_TITLE_WITH_PRICE = '''
-{0.name} - купить в интернет-магазине в СПб оптом, цена от {1.price:.0f} -
+{name} - купить в интернет-магазине в СПб оптом, цена от {price:.0f} руб -
 ShopElectro
 '''.replace('\n', ' ')
 
@@ -17,48 +18,72 @@ CATEGORY_SEO_TEXT_POSTFIX = '''
 '''.replace('\n', ' ')
 
 PRODUCT_TITLE = '''
-{0.name} - купить недорого оптом, со скидкой в интернет-магазине ShopElectro
-в СПб, цена - {0.model.price:.0f}.
+{name} - купить недорого оптом, со скидкой в интернет-магазине ShopElectro
+в СПб, цена - {price:.0f} руб
 '''.replace('\n', ' ')
 
 PRODUCT_DESCRIPTION = '''
-{0.name} - Элементы питания, зарядные устройства, ремонт. Купить
-{0.model.category.name} в Санкт-Петербурге.
+{name} - Элементы питания, зарядные устройства, ремонт. Купить
+{category_name} в Санкт-Петербурге.
 '''.replace('\n', ' ')
 
 
-def get_category_handlers():
-    def update_title(page_):
-        category_products = Product.objects.get_by_category(page_.model)
-        if category_products.exists():
-            product_with_lowest_price = category_products.order_by('price').first()
-            page_.title = CATEGORY_TITLE_WITH_PRICE.format(
-                page_, product_with_lowest_price,
-            )
-        else:
-            page_.title = CATEGORY_TITLE.format(page_)
-        return page_
-
-    def update_seo_text(page_):
-        if CATEGORY_SEO_TEXT_POSTFIX not in page_.seo_text.strip():
-            page_.seo_text = '{} {}'.format(
-                page_.seo_text, CATEGORY_SEO_TEXT_POSTFIX,
-            )
-        return page_
-
-    return update_title, update_seo_text
+def update_category_title(page_):
+    product_with_lowest_price = (
+        Product.objects
+        .get_by_category(page_.model)
+        .order_by('price').first()
+    )
+    page_.title = (
+        CATEGORY_TITLE_WITH_PRICE.format(
+            name=page_.name,
+            price=product_with_lowest_price.price,
+        ) if product_with_lowest_price
+        else CATEGORY_TITLE.format(name=page_.name)
+    )
+    return page_
 
 
-def get_product_handlers():
-    def update_title(page_):
-        page_.title = PRODUCT_TITLE.format(page_)
-        return page_
+def update_category_text(page_):
+    if CATEGORY_SEO_TEXT_POSTFIX not in page_.seo_text.strip():
+        page_.seo_text = ' '.join([
+            page_.seo_text, CATEGORY_SEO_TEXT_POSTFIX,
+        ])
+    return page_
 
-    def update_description(page_):
-        page_.description = PRODUCT_DESCRIPTION.format(page_)
-        return page_
 
-    return update_title, update_description
+def update_product_title(page_):
+    page_.title = PRODUCT_TITLE.format(
+        name=page_.name,
+        price=page_.model.price,
+    )
+    return page_
+
+
+def update_product_description(page_):
+    category_name = (
+        page_.model.category.name
+        if page_.model.category else ''
+    )
+    page_.description = PRODUCT_DESCRIPTION.format(
+        name=page_.name,
+        category_name=category_name
+    )
+    return page_
+
+
+@transaction.atomic
+def chained_update(pages, handlers):
+    def handle(handlers_, page_):
+        result_page = reduce(lambda x, y: y(x), handlers_, page_)
+        result_page.save()
+
+    for page in pages.iterator():
+        handle(handlers, page)
+
+    print('Updated {} meta tags'.format(
+        pages[0].model._meta.verbose_name_plural
+    ))
 
 
 class Command(BaseCommand):
@@ -66,28 +91,27 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         category_pages = (
             CategoryPage.objects
-                .select_related('shopelectro_category')
-                .prefetch_related('shopelectro_category__products')
+            .select_related('shopelectro_category')
+            .prefetch_related('shopelectro_category__products')
         )
 
         product_pages = (
             ProductPage.objects
-                .select_related('shopelectro_product')
-                .prefetch_related('shopelectro_product__category')
+            .select_related('shopelectro_product')
+            .prefetch_related('shopelectro_product__category')
         )
 
-        self.update(product_pages, get_product_handlers())
-        self.update(category_pages, get_category_handlers())
-
-    @transaction.atomic
-    def update(self, pages, handlers):
-        def handle(functions, arg):
-            processed_page = reduce(lambda x, y: y(x), functions, arg)
-            processed_page.save()
-
-        for page in pages.iterator():
-            handle(handlers, page)
-
-        print('Updated {} meta tags'.format(
-            pages[0].model._meta.verbose_name_plural
-        ))
+        chained_update(
+            category_pages,
+            handlers=[
+                update_category_title,
+                update_category_text,
+            ]
+        )
+        chained_update(
+            product_pages,
+            handlers=[
+                update_product_title,
+                update_product_description,
+            ]
+        )
