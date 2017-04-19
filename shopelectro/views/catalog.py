659705a14a8694a42a951aca875e4049cef2ed41
Artemiy <wertins71@gmail.com>
2017-04-19 15:26:45 +0300

    Improve perfomance (#132)

--- shopelectro/views/catalog.py
+++ shopelectro/views/catalog.py
@@ -15,10 +15,7 @@ from images.models import Image
 from pages import views as pages_views
 
 from shopelectro import config
-from shopelectro.config import PRICE_BOUNDS
-from shopelectro.models import (
-    Product, ProductFeedback, Category, CategoryPage as CategoryPageModel, Tag
-)
+from shopelectro import models
 from shopelectro.views.helpers import set_csrf_cookie
 
 PRODUCTS_ON_PAGE_PC = 48
@@ -33,7 +30,7 @@ def get_products_count(request):
 
 # CATALOG VIEWS
 class CategoryTree(catalog.CategoryTree):
-    category_model = Category
+    category_model = models.Category
 
 
 @set_csrf_cookie
@@ -43,10 +40,10 @@ class ProductPage(catalog.ProductPage):
     slug_field = 'vendor_code'
 
     queryset = (
-        Product.objects
-            .filter(category__isnull=False)
-            .prefetch_related('product_feedbacks', 'page__images')
-            .select_related('page')
+        models.Product.objects
+        .filter(category__isnull=False)
+        .prefetch_related('product_feedbacks', 'page__images')
+        .select_related('page')
     )
 
     def get_context_data(self, **kwargs):
@@ -54,13 +51,13 @@ class ProductPage(catalog.ProductPage):
         context = super(ProductPage, self).get_context_data(**kwargs)
         feedbacks = (
             context[self.context_object_name]
-                .product_feedbacks.all()
-                .order_by('-date')
-            )
+            .product_feedbacks.all()
+            .order_by('-date')
+        )
 
         return {
             **context,
-            'price_bounds': PRICE_BOUNDS,
+            'price_bounds': config.PRICE_BOUNDS,
             'feedbacks': feedbacks
         }
 
@@ -75,14 +72,19 @@ class IndexPage(pages_views.CustomPageView):
         mobile_view = get_user_agent(self.request).is_mobile
 
         top_products = (
-            Product.objects
-                .filter(id__in=settings.TOP_PRODUCTS)
-                .prefetch_related('category')
-                .select_related('page')
+            models.Product.objects
+            .filter(id__in=settings.TOP_PRODUCTS)
+            .prefetch_related('category')
+            .select_related('page')
         )
 
-        images = Image.objects.get_main_images_by_pages(top_products.get_pages())
-        categories = Category.objects.get_root_categories_by_products(top_products)
+        images = Image.objects.get_main_images_by_pages(
+            models.ProductPage.objects.filter(
+                shopelectro_product__in=top_products
+            )
+        )
+
+        categories = models.Category.objects.get_root_categories_by_products(top_products)
 
         prepared_top_products = []
         if not mobile_view:
@@ -99,7 +101,9 @@ class IndexPage(pages_views.CustomPageView):
 
 
 def merge_products_and_images(products):
-    images = Image.objects.get_main_images_by_pages(products.get_pages())
+    images = Image.objects.get_main_images_by_pages(
+        models.ProductPage.objects.filter(shopelectro_product__in=products)
+    )
 
     return [
         (product, images.get(product.page))
@@ -124,17 +128,21 @@ class CategoryPage(catalog.CategoryPage):
         sorting_option = config.category_sorting(sorting)
 
         all_products = (
-            Product.objects
-                .prefetch_related('page__images', 'tags', 'tags__group')
-                .select_related('page')
-                .get_by_category(category, ordering=(sorting_option, ))
+            models.Product.objects
+            .prefetch_related('page__images')
+            .select_related('page')
+            .get_by_category(category, ordering=(sorting_option, ))
         )
 
-        group_tags_pairs = Tag.objects.get_group_tags_pairs(all_products.get_tags())
+        group_tags_pairs = models.Tag.objects.get_group_tags_pairs(
+            models.Tag.objects
+            .filter(products__in=all_products)
+            .prefetch_related('group')
+        )
 
-        tags = self.request.GET.get('tags')
-        if tags:
-            all_products = all_products.get_by_tags(tags.split(','))
+        tag_ids = self.request.GET.get('tags')
+        if tag_ids:
+            all_products = all_products.filter(tags__in=tag_ids.split(','))
 
         products = all_products.get_offset(0, products_on_page)
 
@@ -145,7 +153,7 @@ class CategoryPage(catalog.CategoryPage):
             'total_products': all_products.count(),
             'sorting_options': config.category_sorting(),
             'sort': sorting,
-            'tags': tags,
+            'tags': tag_ids,
             'view_type': view_type,
         }
 
@@ -162,19 +170,19 @@ def load_more(request, category_slug, offset=0, sorting=0):
     """
     products_on_page = get_products_count(request)
 
-    category = get_object_or_404(CategoryPageModel, slug=category_slug).model
+    category = get_object_or_404(models.CategoryPage, slug=category_slug).model
     sorting_option = config.category_sorting(int(sorting))
 
     products = (
-        Product.objects
-            .prefetch_related('page__images')
-            .select_related('page')
-            .get_by_category(category, ordering=(sorting_option,))
+        models.Product.objects
+        .prefetch_related('page__images')
+        .select_related('page')
+        .get_by_category(category, ordering=(sorting_option,))
     )
 
-    tags = request.GET.get('tags')
-    if tags:
-        products = products.get_by_tags(tags.split(','))
+    tag_ids = request.GET.get('tags')
+    if tag_ids:
+        products = products.filter(tags__in=tag_ids.split(','))
 
     products = products.get_offset(int(offset), products_on_page)
     view = request.session.get('view_type', 'tile')
@@ -192,14 +200,14 @@ def save_feedback(request):
         return {arg: request.POST.get(arg, '') for arg in args}
 
     product_id = request.POST.get('id')
-    product = Product.objects.filter(id=product_id).first()
+    product = models.Product.objects.filter(id=product_id).first()
     if not (product_id and product):
         return HttpResponse(status=422)
 
     fields = ['rating', 'name', 'dignities', 'limitations', 'general']
     feedback_data = get_keys_from_post(*fields)
 
-    ProductFeedback.objects.create(product=product, **feedback_data)
+    models.ProductFeedback.objects.create(product=product, **feedback_data)
     return HttpResponse('ok')
 
 
@@ -209,7 +217,7 @@ def delete_feedback(request):
         return HttpResponseForbidden('Not today, sly guy...')
 
     feedback_id = request.POST.get('id')
-    feedback = ProductFeedback.objects.filter(id=feedback_id).first()
+    feedback = models.ProductFeedback.objects.filter(id=feedback_id).first()
     if not (feedback_id and feedback):
         return HttpResponse(status=422)
 
@@ -218,8 +226,8 @@ def delete_feedback(request):
 
 
 class ProductsWithoutImages(catalog.ProductsWithoutImages):
-    model = Product
+    model = models.Product
 
 
 class ProductsWithoutText(catalog.ProductsWithoutText):
-    model = Product
+    model = models.Product
