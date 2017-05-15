9c0b25b8b85ff9d94c16a835e9b7c3522edae14e
Dima Veselov <d.a.veselov@yandex.ru>
2017-05-15 13:23:18 +0300

    [quickfix] show debug toolbar in dev env

--- shopelectro/settings/dev.py
+++ shopelectro/settings/dev.py
@@ -11,3 +11,17 @@ SITE_DOMAIN_NAME = 'stage.shopelectro.ru'
 YANDEX_KASSA_LINK = 'https://demomoney.yandex.ru/eshop.xml'
 
 USE_CELERY = False
+
+
+def show_debug_toolbar(request):
+    '''
+    Show debug toolbar even if settings doesn't have INTERNAL_IPS list
+    '''
+    if request.is_ajax():
+        return False
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'shopelectro.settings.dev.show_debug_toolbar'
+}
