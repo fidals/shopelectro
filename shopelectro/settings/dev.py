48593780229f11712a899ccffdb3c6dd0830c9a4
Dima Veselov <d.a.veselov@yandex.ru>
2017-05-15 15:10:09 +0300

    Reset changes in dev settings

--- shopelectro/settings/dev.py
+++ shopelectro/settings/dev.py
@@ -11,17 +11,3 @@ SITE_DOMAIN_NAME = 'stage.shopelectro.ru'
 YANDEX_KASSA_LINK = 'https://demomoney.yandex.ru/eshop.xml'
 
 USE_CELERY = False
-
-
-def show_debug_toolbar(request):
-    '''
-    Show debug toolbar even if settings doesn't have INTERNAL_IPS list
-    '''
-    return True
-
-
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': 'shopelectro.settings.dev.show_debug_toolbar'
-}
