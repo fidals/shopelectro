9dfb55650d3a4f42b524d002430e5de4c4391014
Artemiy Rodionov <wertins71@gmail.com>
2018-06-01 19:14:47 +0300

    Fix tests

--- shopelectro/tests/tests_views.py
+++ shopelectro/tests/tests_views.py
@@ -9,8 +9,9 @@ from functools import partial
 from itertools import chain
 from operator import attrgetter
 from xml.etree import ElementTree as ET
-from urllib.parse import urlparse
+from urllib.parse import urlparse, quote
 
+from bs4 import BeautifulSoup
 from django.conf import settings
 from django.db.models import Q
 from django.http import HttpResponse
@@ -330,10 +331,9 @@ class TestSearch(TestCase):
 
     fixtures = ['dump.json']
     TERM = 'Prod'
+    SIGNLE_RESULT_TERM = '#0'
+    QUOTED_SIGNLE_RESULT_TERM = quote(SIGNLE_RESULT_TERM)
     WRONG_TERM = 'Bugaga'  # it's short for trigram search testing
-
-    def setUp(self):
-        self.product = Product.objects.filter(category__children__isnull=True).first()
 
     def test_search_has_results(self):
         """Search page should contain at least one result for right term."""
@@ -399,22 +399,31 @@ class TestSearch(TestCase):
         self.assertFalse(json_to_dict(response))
         self.assertNotContains(response, term)
 
-    def assert_content_uniqueness(self, content, key):
-        str_content = str(content)
-        self.assertTrue(str_content.find(key) == str_content.rfind(key))
-
     def test_search_has_no_model_pages(self):
         """Search page does not contain page with type=MODEL_TYPE and duplicated content."""
         response = self.client.get(
-            f'/search/?term={self.product.name}',
+            f'/search/?term={self.QUOTED_SIGNLE_RESULT_TERM}',
             follow=True,
         )
-        self.assertEqual(response.status_code, 200)
-        self.assert_content_uniqueness(response.content, self.product.name)
+        result_links = BeautifulSoup(
+            response.content.decode('utf-8'), 'html.parser'
+        ).find_all(class_='search-result-link')
+        self.assertTrue(len(result_links) == 1)
 
     def test_autocomplete_has_no_model_pages(self):
         """Autocomplete does not contain page with type=MODEL_TYPE and duplicated content."""
-        response = self.client.get(f'{reverse("autocomplete")}?term={self.product.name}')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(json_to_dict(response))
-        self.assert_content_uniqueness(response.content, self.product.name)
+        response = self.client.get(
+            f'{reverse("autocomplete")}?term={self.QUOTED_SIGNLE_RESULT_TERM}'
+        )
+        result = list(filter(lambda d: d['type'] == 'category', json_to_dict(response)))
+        self.assertTrue(
+            len(result) == 1
+        )
+
+    def test_admin_autocomplete_has_no_model_pages(self):
+        """Admin autocomplete does not contain page with type=MODEL_TYPE and duplicated content."""
+        response = self.client.get(
+            f'{reverse("admin_autocomplete")}'
+            f'?term={self.QUOTED_SIGNLE_RESULT_TERM}&pageType=category'
+        )
+        self.assertTrue(len(json_to_dict(response)) == 1)
