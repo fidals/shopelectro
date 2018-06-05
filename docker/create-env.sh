84669312c001a08a220e67e1e04ec3729581f8eb
duker <duker33@gmail.com>
2018-06-05 13:21:13 +0300

    stb#167  Add guard if for .env file

--- docker/create-env.sh
+++ docker/create-env.sh
@@ -46,7 +46,7 @@ function create_env_files {
         file_names=$(join_by , ${new_files[@]})
         read -p \
 "Checkout $file_names and configure it.
-Are you ready to continue and build a new .env? [y/n]: " yn
+Are you ready to continue and build a new env and config files? [y/n]: " yn
         if [[ $yn != "y" ]]
         then
             # Give user possibility to refuse on .env generation,
@@ -57,27 +57,38 @@ Are you ready to continue and build a new .env? [y/n]: " yn
         fi
     fi
 
-    local env_doc=\
+    if [[ ! -f ".env" ]]
+    then
+        echo "generate .env file"
+        local env_doc=\
 $'# Both .env and env_files/ are needed because of docker-compose realization.
 # See good explanation here:
 # https://github.com/docker/compose/issues/4223#issuecomment-280077263\n'
-    echo "$env_doc" > .env
+        echo "$env_doc" > .env
 
-    for file in ${new_files[@]}
-    do
-        cat $file >> .env
-    done
+        for file in ${new_files[@]}
+        do
+            cat $file >> .env
+        done
+    else
+        echo ".env file already exists skip it's creation"
+    fi
 }
 
-if ! $QUITE
+if [[ -f ".env" ]]
 then
-    file_names=$(join_by , ${new_files[@]})
-    read -p \
-"Going to rewrite following all env and some django config files:
-Continue? [y/n]: " yn
-    if [[ $yn == "y" ]]
+    if ! $QUITE
     then
-        create_env_files
+        read -p "Env file already exists. Remove it? [y/n]: " yn
+        if [[ $yn = "y" ]]
+        then
+            rm .env
+        fi
+    else
+        rm .env
     fi
 fi
+
+create_env_files
+echo "Env files and config generated successfully."
 exit 0
