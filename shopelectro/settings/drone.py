c0f5c2f3795e0ad5b726d09372d866aa39bfa840
Artemiy Rodionov <wertins71@gmail.com>
2018-07-31 17:59:56 +0300

    Pin celery version. Move SELENIUM_* variables to local.py/drone.py

--- shopelectro/settings/drone.py
+++ shopelectro/settings/drone.py
@@ -12,4 +12,6 @@ SITE_DOMAIN_NAME = 'stage.shopelectro.ru'
 
 YANDEX_KASSA_LINK = 'https://demomoney.yandex.ru/eshop.xml'
 
-USE_CELERY = False
+SELENIUM_URL = os.environ.get('SELENIUM_URL', 'http://selenium:4444/wd/hub')
+SELENIUM_WAIT_SECONDS = int(os.environ['SELENIUM_WAIT_SECONDS'])
+SELENIUM_TIMEOUT_SECONDS = int(os.environ['SELENIUM_TIMEOUT_SECONDS'])
