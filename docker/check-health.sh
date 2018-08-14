ea1fa1fc257afd09d59b2148457c22b28d892404
Artemiy Rodionov <wertins71@gmail.com>
2018-08-14 16:49:03 +0300

    Fix typo

--- docker/check-health.sh
+++ docker/check-health.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-URL=$1s
+URL=$1
 [[ $URL ]] || (echo "Specify an url to be checked as the first argument" && exit 1)
 
 wget -O- $URL -q | grep shopelectro.ru > /dev/null && echo "OK" || exit 1
