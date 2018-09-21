fa22e2dce736e38fcadc715a3fab7acbe2866217
duker33 <duker33@gmail.com>
2018-09-21 12:00:03 +0300

    #583  Apply linter rules

--- shopelectro/admin.py
+++ shopelectro/admin.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.contrib.redirects.models import Redirect
