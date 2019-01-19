de9a7ad474dbe138c9a1238e1d30959846c61c58
Artemiy Rodionov <wertins71@gmail.com>
2019-01-19 22:26:30 +0300

    Remove excess import from base.py

--- shopelectro/settings/base.py
+++ shopelectro/settings/base.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
-import enum
 import os
 import socket
 from datetime import datetime
