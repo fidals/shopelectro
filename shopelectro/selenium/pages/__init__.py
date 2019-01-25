7f0f608f01ee185a05babab7dd2c7dd91fa4324b
Artemiy Rodionov <wertins71@gmail.com>
2019-01-25 21:05:15 +0300

    Create SuccessPage

--- shopelectro/selenium/pages/__init__.py
+++ shopelectro/selenium/pages/__init__.py
@@ -2,3 +2,4 @@ from .page import Page
 
 from .category import CategoryPage
 from .order import OrderPage
+from .success import SuccesPage
---
+++ shopelectro/selenium/pages/success.py
@@ -0,0 +1,13 @@
+from shopelectro.selenium.pages import Page
+
+from pages.models import CustomPage
+
+
+class SuccesPage(Page):
+
+    @property
+    def path(self):
+        CustomPage.objects.get(slug='order-success').url
+
+    def is_success(self):
+        'Заказ принят' in self.driver.find_element_by_tag_name('h1').text
