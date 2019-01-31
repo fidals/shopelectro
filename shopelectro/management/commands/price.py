7a3c98fb2c00fce5d099cf10572e6869bd3e7b53
duker33 <duker33@gmail.com>
2019-01-31 15:53:51 +0300

    #715  Move context processing to separated Products class

--- shopelectro/management/commands/price.py
+++ shopelectro/management/commands/price.py
@@ -48,40 +48,8 @@ class Prices:
     pass
 
 
-# --- data ---
-class PriceFilter:
-    """Filter offers with individual price requirements."""
-
-    FILTERS = defaultdict(
-        lambda: (lambda qs: qs),
-        # Yandex Market feed requires picture for every offer
-        YM=lambda qs: (
-            qs
-            .filter(page__images__isnull=False)
-            .distinct()
-        ),
-        # Google Merchant feed should not contain offers cheaper then CONST
-        GM=lambda qs: (
-            qs
-            .filter(price__gt=settings.PRICE_GM_LOWER_BOUND)
-        )
-    )
-
-    def __init__(self, utm: str):
-        assert utm in settings.UTM_PRICE_MAP
-        self.utm = utm
-
-    def run(self, query_set: QuerySet) -> QuerySet:
-        return self.FILTERS[self.utm](query_set)
-
-
 class Context(newcontext.Context):
     """DB data, extracted for price file."""
-
-    UTM_MEDIUM_DATA = defaultdict(
-        lambda: 'cpc',
-        {'YM': 'cpc-market'}
-    )
 
     def __init__(self, target: str):
         self.target = target
@@ -89,66 +57,17 @@ class Context(newcontext.Context):
     # @todo #666:120m  Split price.Context.context to smaller classes
     #  Don't forget it:
     #  - Move class `PriceFilter` to `Product`
-    #  - Merge to `IGNORED` constants
     #  - Rm constants from Tree class
     #  - Inject logs object here instead of returning operation status
     #  - Maybe split this file to blocks
     def context(self) -> dict:
-        def put_utm(product):
-            """Put UTM attribute to product."""
-            utm_marks = [
-                ('utm_source', self.target),
-                ('utm_medium', self.UTM_MEDIUM_DATA[self.target]),
-                ('utm_content', product.get_root_category().page.slug),
-                ('utm_term', str(product.vendor_code)),
-            ]
-
-            url = reverse('product', args=(product.vendor_code,))
-            utm_mark_query = '&'.join(f'{k}={v}' for k, v in utm_marks)
-            product.utm_url = f'{settings.BASE_URL}{url}?{utm_mark_query}'
-
-            product.prepared_params = [
-                (group, tags[0].name)
-                for (group, tags) in filter(
-                    lambda x: x[0].name != 'Производитель',
-                    product.get_params()
-                ) if tags
-            ]
-
-            return product
-
-        def put_crumbs(product):  # Ignore PyDocStyleBear
-            """Crumbs for google merchant. https://goo.gl/b0UJQp"""
-            product.crumbs = ' > '.join(
-                product.page.get_ancestors_fields('h1', include_self=False)[1:]
-            )
-            return product
-
-        def put_brand(product, brands):
-            product.brand = brands.get(product)
-            return product
-
-        def prepare_products(categories_, utm):
-            """Filter product list and patch it for rendering."""
-            products = PriceFilter(utm).run(
-                models.Product.objects.active()
-                .bind_fields()
-                .filter(category__in=categories_, price__gt=0)
-            )
-            brands = models.Tag.objects.get_brands(products)
-            return [
-                put_brand(put_crumbs(put_utm(product)), brands)
-                for product in products
-            ]
-
         categories = Categories(self.target).filter()
-
-        products = prepare_products(categories, self.target)
+        products = Products(self.target, parents=categories)
 
         return {
             'base_url': settings.BASE_URL,
             'categories': categories,
-            'products': products,
+            'products': products.prepare(),
             'shop': settings.SHOP,
             'utm': self.target,
         }
@@ -179,6 +98,7 @@ class Categories:
         assert target in settings.UTM_PRICE_MAP
         self.target = target
 
+    # TODO - move to filter like Products.FILTERS
     def filter(self) -> models.SECategoryQuerySet:
         if self.target == 'SE78':
             return models.Category.objects.all()
@@ -205,6 +125,83 @@ class Categories:
             result_categories = result_categories.get_categories_tree_with_pictures()
 
         return result_categories
+
+
+class Products:
+    """Filter offers with individual price requirements."""
+
+    FILTERS = defaultdict(
+        lambda: (lambda qs: qs),
+        # Yandex Market feed requires picture for every offer
+        YM=lambda qs: (
+            qs
+            .filter(page__images__isnull=False)
+            .distinct()
+        ),
+        # Google Merchant feed should not contain offers cheaper then CONST
+        GM=lambda qs: (
+            qs
+            .filter(price__gt=settings.PRICE_GM_LOWER_BOUND)
+        )
+    )
+
+    UTM_MEDIUM_DATA = defaultdict(
+        lambda: 'cpc',
+        {'YM': 'cpc-market'}
+    )
+
+    def __init__(self, target: str, parents: models.SECategoryQuerySet):
+        assert target in settings.UTM_PRICE_MAP
+        self.target = target
+        self.parents = parents
+
+    def filter(self) -> QuerySet:
+        return self.FILTERS[self.target](
+            models.Product.objects.active().filter_by_categories(self.parents)
+        )
+
+    def put_utm(self, product):
+        """Put UTM attribute to product."""
+        utm_marks = [
+            ('utm_source', self.target),
+            ('utm_medium', self.UTM_MEDIUM_DATA[self.target]),
+            ('utm_content', product.get_root_category().page.slug),
+            ('utm_term', str(product.vendor_code)),
+        ]
+
+        url = reverse('product', args=(product.vendor_code,))
+        utm_mark_query = '&'.join(f'{k}={v}' for k, v in utm_marks)
+        product.utm_url = f'{settings.BASE_URL}{url}?{utm_mark_query}'
+
+        product.prepared_params = [
+            (group, tags[0].name)
+            for (group, tags) in filter(
+                lambda x: x[0].name != 'Производитель',
+                product.get_params()
+            ) if tags
+        ]
+
+        return product
+
+    def put_crumbs(self, product):  # Ignore PyDocStyleBear
+        """Crumbs for google merchant. https://goo.gl/b0UJQp"""
+        product.crumbs = ' > '.join(
+            product.page.get_ancestors_fields('h1', include_self=False)[1:]
+        )
+        return product
+
+    def put_brand(self, product, brands):
+        product.brand = brands.get(product)
+        return product
+
+    def prepare(self) -> typing.List[models.Product]:
+        """Path every product with additional fields."""
+        products = self.filter()
+        brands = models.Tag.objects.get_brands(products)
+        return [
+            self.put_brand(self.put_crumbs(self.put_utm(product)), brands)
+            for product in products
+        ]
 
 
 # --- command block ---
