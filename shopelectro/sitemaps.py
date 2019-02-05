6f860598f05d773921fd200b7d68477c10025c7c
duker33 <duker33@gmail.com>
2019-02-05 12:31:11 +0300

    rf#253  Use new product queryset methods

--- shopelectro/sitemaps.py
+++ shopelectro/sitemaps.py
@@ -49,7 +49,7 @@ def get_categories_with_tags() -> Generator[
     So, for each tags group in each category we'll get 1 tag.
     """
     for category in Category.objects.filter(page__is_active=True):
-        products = Product.objects.get_by_category(category)
+        products = Product.objects.filter_descendants(category)
         tags = Tag.objects.filter_by_products(products)
         for group_name, group_tags in tags.get_group_tags_pairs():
             for group_tag in group_tags:
