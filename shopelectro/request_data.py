7ae609daaf285957001490f431cfa5d8c5adf8ff
duker33 <duker33@gmail.com>
2019-03-14 11:54:51 +0300

    stb#447  Move Request class to the refarm side

--- shopelectro/request_data.py
+++ shopelectro/request_data.py
@@ -3,18 +3,8 @@ import typing
 from django import http
 from django_user_agents.utils import get_user_agent
 
+from pages.request_data import Request
 from shopelectro.exception import Http400
-
-
-class Request:
-    """Comprehensive request entity: django's request + url arguments."""
-
-    def __init__(
-        self, request: http.HttpRequest, url_kwargs: typing.Dict[str, str]
-    ):
-        """:param request: came here throw django urls and django views."""
-        self.request = request
-        self.url_kwargs = url_kwargs
 
 
 class Catalog(Request):
