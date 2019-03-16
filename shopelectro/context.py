a6d798a145a0aa5a0441de097608eb2c1d2aad42
duker33 <duker33@gmail.com>
2019-03-16 11:09:17 +0300

    #set  of minor fixes related to stb (#773)
    * stb#447  Move Request class to the refarm side
    
    * stb#450  Create ContextDict type at refarm side
    
    * stb#444  Move catalog Page to refarm side
    
    * #fix  Uncomment docker-compose mounted refarm code
    
    * rf#260  Rename "newcontext" to "context"
    
    * #set  Grade refarm version to 0.5.1
    
    * rf#309  Adapt context to the new structure
    
    * #set  Grade refarm to 0.5.2
    
    * #fix  catalog.context structure issue
    
    * #set  Grade refarm to 0.5.3

--- shopelectro/context.py
+++ shopelectro/context.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 
-from catalog import newcontext
+from catalog import context, typing
 from images.models import Image
-from pages import models as pages_models, newcontext as pages_newcontext
+from pages import models as pages_models, context as pages_context
 from shopelectro import models, request_data
 
 
@@ -22,28 +22,7 @@ class SortingOption:
         return self.direction + self.field
 
 
-class Page(newcontext.Context):
-
-    def __init__(self, page, tags: newcontext.Tags):
-        self._page = page
-        self._tags = tags
-
-    def context(self):
-        tags_qs = self._tags.qs()
-        # use dirty patch here, because it's the most simple method
-        # to make shared templates work.
-        # For example `templates/layout/metadata.html`.
-        self._page.display = {
-            'page': self._page,
-            'tag_titles': tags_qs.as_title(),
-            'tags': tags_qs,
-        }
-        return {
-            'page': self._page,
-        }
-
-
-class Catalog(newcontext.Context):
+class Catalog(context.Context):
 
     def __init__(self, request_data_: request_data.Catalog):
         self.request_data = request_data_
@@ -59,15 +38,15 @@ class Catalog(newcontext.Context):
     def category(self):
         return self.page.model
 
-    def context(self) -> dict:
-        all_tags = newcontext.Tags(models.Tag.objects.all())
+    def context(self) -> typing.ContextDict:
+        all_tags = context.Tags(models.Tag.objects.all())
 
-        selected_tags = newcontext.tags.ParsedTags(
+        selected_tags = context.tags.ParsedTags(
             tags=all_tags,
             raw_tags=self.request_data.tags,
         )
         if self.request_data.tags:
-            selected_tags = newcontext.tags.Checked404Tags(selected_tags)
+            selected_tags = context.tags.Checked404Tags(selected_tags)
 
         products = (
             models.Product.objects.active()
@@ -83,22 +62,22 @@ class Catalog(newcontext.Context):
         It's not the most strong place of Django ORM, of course.
         :return: ProductsContext with paginated QuerySet inside
         """
-        # @todo #683:30m Remove *Tags and *Products suffixes from catalog.newcontext classes.
+        # @todo #683:30m Remove *Tags and *Products suffixes from catalog.context classes.
         #  Rename Checked404Tags to ExistingOr404.
-        paginated = newcontext.products.PaginatedProducts(
+        paginated = context.products.PaginatedProducts(
             products=products,
             url=self.request_data.request.path,
             page_number=self.request_data.pagination_page_number,
             per_page=self.request_data.pagination_per_page,
         )
 
-        images = newcontext.products.ProductImages(paginated.products, Image.objects.all())
-        brands = newcontext.products.ProductBrands(paginated.products, all_tags)
-        grouped_tags = newcontext.tags.GroupedTags(
-            tags=newcontext.tags.TagsByProducts(all_tags, products)
+        images = context.products.ProductImages(paginated.products, Image.objects.all())
+        brands = context.products.ProductBrands(paginated.products, all_tags)
+        grouped_tags = context.tags.GroupedTags(
+            tags=context.tags.TagsByProducts(all_tags, products)
         )
-        page = Page(self.page, selected_tags)
-        category = newcontext.category.Context(self.category)
+        page = context.Page(self.page, selected_tags)
+        category = context.category.Context(self.category)
         params = {
             'view_type': self.request_data.get_view_type(),
             'sorting_options': settings.CATEGORY_SORTING_OPTIONS.values(),
@@ -108,7 +87,7 @@ class Catalog(newcontext.Context):
 
         return {
             **params,
-            **pages_newcontext.Contexts([
+            **pages_context.Contexts([
                 page, category, paginated,
                 images, brands, grouped_tags
             ]).context()
