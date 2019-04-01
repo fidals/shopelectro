9c109fd1f191df18aeeda0e4a8f889c05a4cab34
Artemiy Rodionov <wertins71@gmail.com>
2019-04-01 12:06:41 +0300

    Create ProductPage for selenium tests

---
+++ shopelectro/selenium/pages/product.py
@@ -0,0 +1,19 @@
+import typing
+
+from django.urls import reverse
+
+from shopelectro.selenium.pages import Page
+
+
+# @todo #682:120m Implement and reuse shopelectro.selenium.ProductPage for selenium tests.
+
+
+class ProductPage(Page):
+
+    def __init__(self, driver, vendor_code):
+        super().__init__(driver)
+        self.vendor_code = vendor_code
+
+    @property
+    def path(self):
+        return reverse('product', args=(self.vendor_code,))
