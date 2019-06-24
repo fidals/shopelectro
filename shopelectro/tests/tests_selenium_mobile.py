824755a0cf3bcb4fe6cbd81ce459deef91d4ef2e
Artemiy Rodionov <wertins71@gmail.com>
2019-06-25 01:39:39 +0300

    Unskip test_cart

--- shopelectro/tests/tests_selenium_mobile.py
+++ shopelectro/tests/tests_selenium_mobile.py
@@ -158,12 +158,6 @@ class Mobile(MobileSeleniumTestCase):
 
         self.assertFalse(any(t.is_displayed() for t in tags))
 
-    # @todo #843:60m  Fix mobile cart.
-    #  Now it's test fails
-    @unittest.skip
-    # CarrotQuest outer js service produces error on this test.
-    # Enabled debug makes it off.
-    @override_settings(DEBUG=True, INTERNAL_IPS=tuple())
     def test_cart(self):
         """Cart should updated after Product buy."""
         def get_cart_price_and_size():
