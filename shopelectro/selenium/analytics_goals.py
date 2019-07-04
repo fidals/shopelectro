dab691fdaa5f021d24486249d25bc794aa7f1ddb
Artemiy Rodionov <wertins71@gmail.com>
2019-07-04 12:45:36 +0300

    Create Goals interface for selenium tests

---
+++ shopelectro/selenium/analytics_goals.py
@@ -0,0 +1,47 @@
+import abc
+
+from shopelectro.selenium import SiteDriver
+
+# @todo #929:30m Implement GoogleEcommerceGoals and YandexMetrikaGoals and use them in tests.
+
+
+class Goals(abc.ABC):
+    """Front-end reached goalss."""
+
+    def __init__(self, driver: SiteDriver):
+        self.driver = driver
+
+    @abc.abstractmethod
+    def fetch(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __iter__(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __getitem__(self, index: int):
+        raise NotImplementedError
+
+
+class YandexEcommerceGoals(Goals):
+    """
+    Represent reached Yandex ecommerce goals.
+
+    Unwrap an excess nesting level common for every goal:
+    [{"ecommerce": ...}] -> ...
+    Structure docs: https://yandex.com/support/metrica/data/e-commerce.html
+    """
+    def __init__(self, driver: SiteDriver):
+        super().__init__(driver)
+        self.goals = []
+
+    def fetch(self):
+        self.goals = self.driver.execute_script('return window.dataLayer.results;')
+
+    def __getitem__(self, index: int):
+        return self.goals[index][0]['ecommerce']
+
+    def __iter__(self):
+        for g in self.goals:
+            yield g[0]['ecommerce']
