10d3c3688aafe1baa0745c4ad9dfa3294a809581
Artemiy Rodionov <wertins71@gmail.com>
2019-07-09 20:46:52 +0300

    Create Positions selenium element

---
+++ shopelectro/selenium/elements/positions.py
@@ -0,0 +1,43 @@
+from contextlib import contextmanager
+
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+
+from shopelectro.selenium import SiteDriver, elements
+
+
+class Positions:
+
+    def __init__(self, driver: SiteDriver, position_type: elements.Product, locator):
+        self.driver = driver
+        self.position_type = position_type
+        self.locator = locator
+
+    @contextmanager
+    def wait_changes(self):
+        def are_changed(_):
+            try:
+                return positions_before != self.all()
+            except TimeoutException:
+                # An exception can be raised from a position's equality method.
+                # In most cases this means that some positions are stale,
+                # so we return False in order to re-execute this wait function.
+                return False
+
+        positions_before = self.all()
+        yield
+        self.driver.wait.until(are_changed)
+
+    def first(self) -> elements.Product:
+        return self.position_type(self.driver, 0)
+
+    def all(self) -> [elements.Product]:
+        try:
+            # use short_wait to avoid long pauses in case of the empty cart
+            positions_count = len(self.driver.short_wait.until(EC.presence_of_all_elements_located(
+                self.locator
+            )))
+        except TimeoutException:
+            positions_count = 0
+
+        return [self.position_type(self.driver, i) for i in range(positions_count)]
