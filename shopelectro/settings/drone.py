b5aa634ec0d2a0493b10295cfc3bc831b7bde4d3
duker33 <duker33@gmail.com>
2019-10-09 21:00:48 +0300

    #991  Disable django app cache for drone CI

--- shopelectro/settings/drone.py
+++ shopelectro/settings/drone.py
@@ -5,6 +5,12 @@ from .base import *
 
 DEBUG = True
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
 # http://bit.ly/sorl-thumbnail-docs
 THUMBNAIL_DEBUG = True
 
